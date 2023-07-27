136cc6a59703442e140bf0835b4b0d3c2b0637b4
mohamadHajAhmad <mohamad30002468@gmail.com>
2023-07-27 13:12:09 +0300

    repairs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,28 +1,4 @@
 ﻿using DataOperation;
 using Models;
 
-PartOperation.Add("sss", 333, 23);
-
-    CarOperation.Add("fddfd", DateTime.Now, "fdfd", 55, new List<Part>()
-    {
-        new Part()
-        {
-            Name = "dd",
-            Price = 22 ,
-        }
-    });
-    SupplierOperation.Add("ddssd", "Dfdf", new List<Part>()
-    {
-        new Part()
-        {
-            Name = "dd",
-            Price = 22 ,
-        }
-    });
-string[] arr = new string[]
-    {
-                "sdsd" ,
-                "ddsds ",
-                "sdsda"
-    };
-List<string> list = new List<string>(arr);
+Console.WriteLine("mohamad Haj Ahmad");
